2e2277770db5d6b726229419702b68ba1f67eb7b
Rachael Sawyer <Rachael@Benjamins-iMac.hsd1.wa.comcast.net>
2020-07-16 22:59:37 -0700

    Add new file

---
+++ Lesson02/calculator.js
@@ -0,0 +1,30 @@
+var x = 200;
+console.log(x * 4);
+
+x = 800
+
+
+let x = 57;
+if (x / 4 >= 9) {   57>=9
+  console.log(x)
+}
+
+// 57
+
+
+let x = 'red';
+let y = 'balloon';
+
+if (20 / 4 === 4) {
+  console.log(x);
+} else if (x === 'balloon'){
+  console.log(y);
+} else if (y === 'balloon') {
+  console.log('finally');
+}
+
+// finally
+
+var x = (5 * (60 + 12)) + 20;
+
+x = 380
